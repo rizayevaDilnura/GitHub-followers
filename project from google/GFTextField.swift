78735d545c4e3bd1c94e12fd44422be4e7a53fc8
Dilnura Rizayeva <didi.riri2004@mail.ru>
2024-03-21 14:03:16 +0300

    searchVc and custom alert

---
+++ project from google/GFTextField.swift
@@ -0,0 +1,39 @@
+//
+//  GFTextField.swift
+//  project from google
+//
+//  Created by Dilnura Rizaeva on 21.03.2024.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configure() {
+translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+
+        placeholder = "Enter a username"
+    }
+
+}
