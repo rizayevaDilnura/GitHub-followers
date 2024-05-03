59712c06ac0f0aac6f09ee971753bd1498376486
Dilnura Rizayeva <didi.riri2004@mail.ru>
2024-05-03 22:43:34 +0300

    planning network call

---
+++ project from google/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  project from google
+//
+//  Created by Dilnura Rizaeva on 03.05.2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ project from google/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  project from google
+//
+//  Created by Dilnura Rizaeva on 03.05.2024.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
