df2821a8816faf222056fd11b3145659739ff25a
Dilnura Rizayeva <didi.riri2004@mail.ru>
2024-05-05 23:36:04 +0300

    create network call = get followers

--- project from google/Screens/FollowerListVC.swift
+++ project from google/Screens/FollowerListVC.swift
@@ -15,7 +15,19 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
+
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Kinda errror", message: errorMessage!, buttonTitle: "OK")
+                return
+            }
+
+            print("Followers.count = \(followers.count)")
+            print(followers)
+
+        }
     }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
