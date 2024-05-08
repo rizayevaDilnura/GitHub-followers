da26ab0c668b5c5b29676e4bdcf242d08eaf1846
Dilnura Rizayeva <didi.riri2004@mail.ru>
2024-05-08 21:02:57 +0300

    result type refactor

--- project from google/Screens/FollowerListVC.swift
+++ project from google/Screens/FollowerListVC.swift
@@ -16,23 +16,19 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
 
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Kinda errror", message: errorMessage?.rawValue!, buttonTitle: "OK")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "There is an error", message: error.rawValue, buttonTitle: "OK")
             }
-
-            print("Followers.count = \(followers.count)")
-            print(followers)
-
         }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
-
     }
-
-
 }
