8a7c7d87fafd2c24779bc9f22b28a992024a36f9
Dilnura Rizayeva <didi.riri2004@mail.ru>
2024-06-03 14:18:45 +0300

    flowLayout

--- project from google/Screens/FollowerListVC.swift
+++ project from google/Screens/FollowerListVC.swift
@@ -10,12 +10,47 @@ import UIKit
 class FollowerListVC: UIViewController {
 
     var username: String!
-    
-    override func viewDidLoad() {
-        view.backgroundColor = .systemBackground
-        super.viewDidLoad()
-        navigationController?.navigationBar.prefersLargeTitles = true
+    var collectionView: UICollectionView!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeeColumnFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+
+    func createThreeeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpace: CGFloat = 10
+        let availableWidth = width -  (padding * 2) - (minimumItemSpace * 2)
+        let itemWidth = availableWidth / 3
+
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+
+        return flowLayout
+    }
+
+    func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             switch result {
             case .success(let followers):
@@ -25,10 +60,5 @@ class FollowerListVC: UIViewController {
                 self.presentGFAlertOnMainThread(title: "There is an error", message: error.rawValue, buttonTitle: "OK")
             }
         }
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 }
