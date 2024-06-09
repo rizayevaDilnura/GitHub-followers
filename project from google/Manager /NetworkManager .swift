8d5f26b5f3ae79460e3428e4e44587287bbf7cf3
Dilnura Rizayeva <didi.riri2004@mail.ru>
2024-06-10 00:34:33 +0300

    downloading and caching avatar images

--- project from google/Manager /NetworkManager .swift
+++ project from google/Manager /NetworkManager .swift
@@ -5,11 +5,12 @@
 //  Created by Dilnura Rizaeva on 05.05.2024.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
 
     private init() {}
 
